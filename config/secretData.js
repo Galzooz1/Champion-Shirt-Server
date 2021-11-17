8737f9bece494fac1da43683d8afa7f6f9685556
Gal <galzooz1@gmail.com>
2021-11-17 20:54:45 +0200

    Remove the now ignored directory some-directory

--- config/secretData.js
+++
@@ -1,9 +0,0 @@
-exports.config = {
-    jwtSecret: "monkeysSecret",
-    mongoPassword: "monkey123",
-    mongoUser: "Galzooz1"
-}
-exports.configEmail = {
-    myEmail: "galzooz1@gmail.com",
-    myPassword: "94989498"
-}
